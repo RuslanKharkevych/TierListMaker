3a1fe78f98ac7a915e655d5e83d012db119d8f78
RuslanKharkevych <kharkevychruslan@gmail.com>
2023-06-04 22:10:57 +0300

    Implement About screen UI

--- app/src/main/java/me/khruslan/tierlistmaker/ui/screens/home/FeedbackFragment.kt
+++ app/src/main/java/me/khruslan/tierlistmaker/ui/screens/home/FeedbackFragment.kt
@@ -3,15 +3,13 @@ package me.khruslan.tierlistmaker.ui.screens.home
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
-import androidx.annotation.StringRes
 import androidx.core.net.toUri
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.snackbar.Snackbar
 import me.khruslan.tierlistmaker.BuildConfig
 import me.khruslan.tierlistmaker.R
 import me.khruslan.tierlistmaker.utils.view.FeedbackUtils
-import timber.log.Timber
+import me.khruslan.tierlistmaker.utils.view.setOnPreferenceClickListener
 
 /**
  * [PreferenceFragmentCompat] that represents "Feedback" section in the navigation drawer.
@@ -40,46 +38,6 @@ class FeedbackFragment : PreferenceFragmentCompat() {
         setOnPreferenceClickListener(R.string.pref_contact_us_key) { sendFeedback() }
         setOnPreferenceClickListener(R.string.pref_report_bug_key) { reportBug() }
         setOnPreferenceClickListener(R.string.pref_rate_app_key) { rateApp() }
-    }
-
-    /**
-     * Sets click listener for the preference found by the key resolved from [keyResId]. If the
-     * preference is not found, logs error.
-     *
-     * @param keyResId string resource identifier of the preference key.
-     * @param onClick the callback to be invoked when the preference is clicked.
-     */
-    private fun setOnPreferenceClickListener(@StringRes keyResId: Int, onClick: () -> Unit) {
-        try {
-            findPreference(keyResId).setOnClickListener(onClick)
-        } catch (e: PreferenceNotFoundException) {
-            Timber.e(e, "Invalid preference configuration")
-        }
-    }
-
-    /**
-     * Finds preference by the key resolved from [keyResId].
-     *
-     * @param keyResId string resource identifier of the preference key.
-     * @return found preference.
-     * @throws [PreferenceNotFoundException] when preference was not found.
-     */
-    private fun findPreference(@StringRes keyResId: Int): Preference {
-        val key = getString(keyResId)
-        return findPreference(key) ?: throw PreferenceNotFoundException(key)
-    }
-
-    /**
-     * Sets click listener for this preference.
-     *
-     * @receiver clickable preference.
-     * @param onClick the callback to be invoked when this preference is clicked.
-     */
-    private fun Preference.setOnClickListener(onClick: () -> Unit) {
-        setOnPreferenceClickListener {
-            onClick()
-            true
-        }
     }
 
     /**
@@ -132,12 +90,4 @@ class FeedbackFragment : PreferenceFragmentCompat() {
             .make(requireView(), R.string.snackbar_msg_no_rate_apps_found, Snackbar.LENGTH_LONG)
             .show()
     }
-
-    /**
-     * Thrown to indicate that the preference was not found.
-     *
-     * @param preferenceKey key of the preference.
-     */
-    private class PreferenceNotFoundException(preferenceKey: String) :
-        Exception("Preference with key $preferenceKey not found")
 }
