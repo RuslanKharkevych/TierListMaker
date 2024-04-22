97d86714b9029823921c172009313535027454a2
RuslanKharkevych <kharkevychruslan@gmail.com>
2024-04-22 03:03:02 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,20 @@
+<img alt="App icon" src="app/src/main/ic_launcher-playstore.png" width="20px" height="20px"
+align="center" /> **Tier List Maker** is an Android application that allows to create, edit and
+share [tier lists](https://en.wikipedia.org/wiki/Tier_list).
+
+## Features
+
+- Сreate and edit tier lists
+- Upload images from device storage
+- Choose between light and dark theme
+- Share tier list as image file
+
+![Features overview](screenshots/features_overview.png)
+
+## Documentation
+
+| Module | Description |
+|---|---|
+| [app](docs/app/index.md) | Tier List Maker application module. This is the only Android module in the project. |
+| [dokka](docs/app/dokka/index.md) | Kotlin library for custom Dokka plugin. Submodule of the app. |
+| [lint](docs/app/lint/index.md) | Kotlin library for custom lint checks. Submodule of the app. |
--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -2,21 +2,41 @@ set -e
 cd "$(realpath "$(dirname "$0")")"
 cd "../"
 
-DOCS_PATH='docs'
-BACKUP_PATH="build/tmp/$DOCS_PATH"
+DOCS_FOLDER_PATH='docs'
+TEMP_FOLDER_PATH='build/tmp'
+DOCS_BACKUP_FOLDER_PATH="$TEMP_FOLDER_PATH/$DOCS_FOLDER_PATH"
 
 backup_created=false
-if [ -d "$DOCS_PATH" ]; then
-  mkdir -p "$BACKUP_PATH"
-  mv "$DOCS_PATH" "$BACKUP_PATH"
+if [ -d "$DOCS_FOLDER_PATH" ]; then
+  mkdir -p "$DOCS_BACKUP_FOLDER_PATH"
+  mv "$DOCS_FOLDER_PATH" "$DOCS_BACKUP_FOLDER_PATH"
   backup_created=true
 fi
 
 if ./gradlew :dokkaGfmMultiModule; then
-  git add "$DOCS_PATH"
+  git add "$DOCS_FOLDER_PATH"
   if [ "$backup_created" = true ]; then
-    rm -r "$BACKUP_PATH"
+    rm -r "$DOCS_BACKUP_FOLDER_PATH"
   fi
 elif [ "$backup_created" = true ]; then
-  mv "$BACKUP_PATH" "$DOCS_PATH"
+  mv "$DOCS_BACKUP_FOLDER_PATH" "$DOCS_FOLDER_PATH"
+  exit 1
 fi
+
+ROOT_DOC_FILE_PATH="$DOCS_FOLDER_PATH/index.md"
+README_FILE_PATH='README.md'
+TEMP_FILE_PATH="$TEMP_FOLDER_PATH/temp.md"
+
+root_doc_file_text=''
+while IFS= read -r line; do
+  case $line in \|*)
+    refined_row=$(echo "$line" | sed "s,\(\[[^]]*\]\)(\([^)]*\)),\1($DOCS_FOLDER_PATH/\2),g" | \
+    sed -e 's/Name/Module/' -e 's/| |/| Description |/')
+    root_doc_file_text+="\n$refined_row"
+    ;;
+  esac
+done <$ROOT_DOC_FILE_PATH
+
+awk -v pattern='## Documentation' -v text="$root_doc_file_text" \
+'$0 ~ pattern {print; print text; exit} 1' \
+$README_FILE_PATH > $TEMP_FILE_PATH && mv $TEMP_FILE_PATH $README_FILE_PATH
